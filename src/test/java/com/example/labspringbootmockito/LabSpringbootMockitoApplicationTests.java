179ecacceb3f510a0625faa50c44ec38cc4a7234
DJ Garcia <dedjer@gmail.com>
2018-10-13 23:29:17 -0500

    Added example of mocking without annotations

--- src/test/java/com/example/labspringbootmockito/LabSpringbootMockitoApplicationTests.java
+++ src/test/java/com/example/labspringbootmockito/LabSpringbootMockitoApplicationTests.java
@@ -1,19 +1,23 @@
 package com.example.labspringbootmockito;
 
+import com.example.labspringbootmockito.Controller.Bank;
 import com.example.labspringbootmockito.Controller.Calculator;
+import com.example.labspringbootmockito.Service.BankService;
 import com.example.labspringbootmockito.Service.CalculatorService;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,23 +29,46 @@ public class LabSpringbootMockitoApplicationTests {
 	@InjectMocks
 	private Calculator calculator;
 
+	private BankService bankService;
+
+	private Bank bank;
+
 	@Before
-	public void before() {
-//		calculatorService = new CalculatorService();
-//		calculator = new Calculator(calculatorService);
+	public void setUp() throws Exception {
+		// Mock without using annotations
+		bankService = mock(BankService.class);
+		bank = new Bank(bankService);
 	}
 
 	@Test
 	public void whenAddTwoNumbersTest(){
 
-		// If anything calls the calculatorService.Add passing in 4, 4 then return 9
-		Mockito.when(calculatorService.Add(4,4)).thenReturn(8);
+		// arrange
+		int x = 4;
+		int y = 4;
+		int result = 8;
 
-		// Test calculator.add method
-		assertThat(calculator.Add(4, 4), is(equalTo(8)));
+		// act
+		when(calculatorService.Add(x,y)).thenReturn(result);
 
-		// Verify that my mocked calculatorService.Add method was called sometime during the test
-		Mockito.verify(calculatorService).Add(4,4);
+		// assert
+		assertThat(calculator.Add(x, y), is(equalTo(result)));
+		verify(calculatorService).Add(x,y);
 	}
 
+	@Test
+	public void verifyBalanceAfterDepositTest() {
+
+		//arrange
+		long account = 12345678;
+		double amount = 1000.23;
+		double balance = 2000.23;
+
+		//act
+		when(bankService.Deposit(account, amount)).thenReturn(balance);
+
+		//assert
+		assertThat(bank.Deposit(account, amount), is(equalTo(balance)));
+		verify(bankService).Deposit(account, amount);
+	}
 }
